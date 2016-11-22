1ca74af67314827fa384ce18ddf3d90317ce94f0
scollis <scollis.acrf@gmail.com>
2016-11-22 14:27:23 -0600

    INIT: First commit

---
+++ cababorr/process_cpol_bom.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""
+cababorr.process_cpol_bom
+=========================
+top level script for processing a cpol file from command line
+on systems at the Australian Bureau of Meteorology
+
+.. autosummary::
+    :toctree: generated/
+    hello_world
+"""
+
+import processing_code as radar_codes
+
+def hello_world():
+    #placeholder file
+    print('why hello BoM')
+    radar_codes.hello_world()
+
+if __name__ == "__main__":
+    hello_world()
---
+++ cababorr/processing_code.py
@@ -0,0 +1,19 @@
+"""
+cababorr.processing code
+=========================
+Common core processing code across the collaboration
+
+.. autosummary::
+    :toctree: generated/
+    hello_world
+"""
+
+import pyart
+
+WAVE_VERSION = 'First'
+
+def hello_world():
+    #placeholder file
+    print('why hello ', WAVE_VERSION, ' wave')
+
+
